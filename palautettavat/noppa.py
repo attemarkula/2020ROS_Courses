24b3628a20bc0c6a9d19d69c572a60faa3bd4e61
Atte Markula <atte.markula@gmail.com>
2020-11-06 22:11:21 +0200

    noppa tehtävän helppo ja normaali

--- palautettavat/noppa.py
+++ palautettavat/noppa.py
@@ -2,24 +2,85 @@
 # -*- coding: utf-8 -*-
 import random
 
-noppa_d100 =[i for i in range(1, 101)]
-print(noppa_d100)
+if False:
+    noppa_d100 =[i for i in range(1, 101)]
+    print(noppa_d100)
+    print(random.choice(noppa_d100))
+    print(noppa_d100[random.randrange(100)])
 
-print(random.choice(noppa_d100))
-print(noppa_d100[random.randrange(100)])
+def nopantahot(tahomaara=0):
+    """ palauta tahomäärän mukaiset nopan silmäluvut listana """
+    #print("nopantahot("+str(tahomaara)+")")
+    lista=[]
 
-#helppo
-#Kysy kauttajalta kuinka monta tahoista noppaa kayttaja haluaa heittaa.
-#Tee funtio joka ottaa sisaansa tahojen lukumaaran ja palauttaa listan jossa on nopan silmaluvut.
-#Anna satunnainen nopantulos printtaamalla se kayttajalle.
+    for target_list in range(tahomaara):
+        lista.append(1+target_list)
+    #print(lista)
+    return lista
 
-#normaali
-#sama kuin normaali, mutta kysy kuinka monta noppaa kayttaja haluaa heittaa, ja printtaa
-# noppien tulos erikseen ja summana
+def noppa(tahomaara):
+    """
+    palauta satunnainen nopan silmäluku
+    """
+    #print("noppa("+str(tahomaara)+"):",end="")
+    
+    vastaus=1+random.randrange(tahomaara)
+    #print(vastaus)
+    return vastaus
 
-#vaikea
-#Sama kuin normaali, mutta tuloksen jalkeen kysy kayttajalta halutaanko heittaa uudelleen.
-#entterilla uusi heitto
-#muuta noppa kysyy uudelleen tahot
-#muuta noppien lukumaara kysyy noppaluvun uudelleen
-#lopeta lopettaa ohjelman
+def helppo(mode="print"):
+    if mode=="print":
+        print("""
+        helppo:
+        Kysy kauttajalta kuinka monta tahoista noppaa kayttaja haluaa heittaa.
+        Tee funtio joka ottaa sisaansa tahojen lukumaaran ja palauttaa listan jossa on nopan silmaluvut.
+        Anna satunnainen nopantulos printtaamalla se kayttajalle.
+        """)
+    tahot=int(input('Montako tahoa nopassa:'))
+    #tahot=6
+    tahot_lista=nopantahot(tahot)
+    np=noppa(tahot)
+    print ("lista silmäluvuista:"+str(tahot_lista))
+    print ("nopan tulos:"+str(np))
+
+
+#def helppo(mode) ends
+
+def normaali(mode="print"):
+    if mode=="print":
+        print ("""
+        normaali
+        sama kuin normaali, mutta kysy kuinka monta noppaa kayttaja haluaa heittaa, ja printtaa
+        noppien tulos erikseen ja summana
+        """)
+    lista=[]
+    nopatlkm=int(input('Montako noppaa:'))
+    tahot=int(input('Montako tahoa nopassa:'))
+    #tahot=100
+    #nopatlkm=15
+    summa=0
+
+    for i in range(nopatlkm):
+        lista.append(noppa(tahot))
+        summa+=lista[i]
+
+    print("summa:"+str(summa)+", silmäluvut: ",end="")
+    print (lista)
+#def normaali(mode) ends
+
+def vaikea(mode="print"):
+    if mode=="print":
+        print ("""
+        #vaikea
+        Sama kuin normaali, mutta tuloksen jalkeen kysy kayttajalta halutaanko heittaa uudelleen.
+        entterilla uusi heitto
+        muuta noppa kysyy uudelleen tahot
+        muuta noppien lukumaara kysyy noppaluvun uudelleen
+        lopeta lopettaa ohjelman
+        """)
+#def vaikea(mode) ends
+
+if __name__ == "__main__":
+#    helppo("noprint")
+   normaali("noprint")
+#   vaikea("noprint")
