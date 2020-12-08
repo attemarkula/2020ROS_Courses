c6dff5f488281a2cc2340f192388d6f3db4569cc
Atte Markula <atte.markula@gmail.com>
2020-12-08 12:10:37 +0200

    randomwalker init

---
+++ catkin_ws/src/autoturtle/src/randomwalker_wallavoider_node.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import rospy
+from geometry_msgs import Twist
+
+#koska subsc paikkatietoon
+from turtlesim.msg import Pose
+
+import random
+
+#callback funktio ajetaan, kun Pose lähettää arvon.
+
+def callback(data):
+    """
+    datan saapuessa suoritettava funktio,
+    koodi välttää seiniä, seuraamalla pose.x arvoa.
+
+    """
+    vel_msg = Twist()
+    pose = data
+
+    if pose.x > 10:
+        vel_msg.angular.z = 0
+        vel_msg.linear.x  = -2
+    elif pose.x < 10:
+        vel_msg.angular.z = 0
+        vel_msg.linear.x  = -2
+    elif pose.x < 1:
+        vel_msg.angular.z = 0
+        vel_msg.linear.x  = -2
+    else:
+        vel_msg.angular.z = random.randint(-2,2)
+        vel_msg.linear.x  = random.randint(0,2)
+    
+    velocity_publisher.publish(vel_msg)
+
+
+velocity_publisher = rospy.Publisher('/turtle1/cmd_ve', Twist, queue_size=1)
+pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, callback)
+
+while not rospy.is_shutdown():
+    rospy.spin()
