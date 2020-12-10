3bb46696561f0a52787003893e75828da8d8d14c
Atte Markula <atte.markula@gmail.com>
2020-12-10 13:48:21 +0200

    autoturtle palautettava helppo

--- catkin_ws/src/autoturtle/src/randomwalker_wallavoider_node.py
+++ catkin_ws/src/autoturtle/src/randomwalker_wallavoider_node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import rospy
-from geometry_msgs import Twist
+from geometry_msgs.msg import Twist
 
 #koska subsc paikkatietoon
 from turtlesim.msg import Pose
@@ -14,7 +14,6 @@ def callback(data):
     """
     datan saapuessa suoritettava funktio,
     koodi välttää seiniä, seuraamalla pose.x arvoa.
-
     """
     vel_msg = Twist()
     pose = data
@@ -22,21 +21,26 @@ def callback(data):
     if pose.x > 10:
         vel_msg.angular.z = 0
         vel_msg.linear.x  = -2
-    elif pose.x < 10:
-        vel_msg.angular.z = 0
-        vel_msg.linear.x  = -2
     elif pose.x < 1:
         vel_msg.angular.z = 0
+        vel_msg.linear.x  = +2
+    elif pose.y > 10:
+        vel_msg.angular.z = 0
         vel_msg.linear.x  = -2
+    elif pose.y < 1:
+        vel_msg.angular.z = 0
+        vel_msg.linear.x  = +2
     else:
         vel_msg.angular.z = random.randint(-2,2)
         vel_msg.linear.x  = random.randint(0,2)
-    
+
     velocity_publisher.publish(vel_msg)
 
-
-velocity_publisher = rospy.Publisher('/turtle1/cmd_ve', Twist, queue_size=1)
+rospy.init_node('turtlebot_auto', anonymous=True)
+velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
 pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, callback)
 
 while not rospy.is_shutdown():
     rospy.spin()
+
+
